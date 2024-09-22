c569720da8cf3aaf601f9b6f57c046dac272a334
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-23 02:12:04 +0600

    Add a crud operations repository.

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
 using Catalog.API.Models;
+using Catalog.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<CatalogDatabaseSettings>(
     builder.Configuration.GetSection("CatalogDatabase"));
+
+builder.Services.AddSingleton<CatalogRepository>();
 
 var app = builder.Build();
 
---
+++ src/Services/Catalog.API/Repository/CatalogRepository.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.API.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repository
+{
+    public class CatalogRepository 
+    {
+        private readonly IMongoCollection<Product> _products;
+        public CatalogRepository(IOptions<CatalogDatabaseSettings> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(options.Value.DatabaseName);
+
+            _products = mongoDatabase.GetCollection<Product>(
+                options.Value.CollectionName);
+        }
+
+        public async Task<List<Product>> GetAsync() =>
+            await _products.Find(_ => true).ToListAsync();
+
+        public async Task<Product?> GetByIdAsync(string id) =>
+            await _products.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task<List<Product>> GetByCategoryAsync(string category) =>
+            await _products.Find(x => x.Category == category).ToListAsync();
+
+        public async Task CreateAsync(Product newProduct) =>
+            await _products.InsertOneAsync(newProduct);
+
+        public async Task UpdateAsync(string id, Product updatedProduct) =>
+            await _products.ReplaceOneAsync(x => x.Id == id, updatedProduct);
+
+        public async Task RemoveAsync(string id) =>
+            await _products.DeleteOneAsync(x => x.Id == id);
+    }
+}
