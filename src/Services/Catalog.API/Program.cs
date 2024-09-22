5e3636301f57d3d8578a2b3c7143c04b5acf2016
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-23 01:23:44 +0600

    Added a configuration model.

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+using Catalog.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<CatalogDatabaseSettings>(
+    builder.Configuration.GetSection("CatalogDatabase"));
 
 var app = builder.Build();
 
