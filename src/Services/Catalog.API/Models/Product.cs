7c9757ff22751ed50d032c774fb02e75e6a9418f
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-23 01:05:24 +0600

    Added Product class to Model folder.

---
+++ src/Services/Catalog.API/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Catalog.API.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+        [BsonElement("description")]
+        public string Description { get; set; } = null!;
+        [BsonElement("category")]
+        public string Category { get; set; } = null!;
+        [BsonElement("summary")]
+        public string Summary { get; set; } = null!;
+        [BsonElement("imageFile")] 
+        public string ImageFile { get; set; } = null!;
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
