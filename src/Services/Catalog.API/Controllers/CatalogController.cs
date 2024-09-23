4fe8b5532887a030261f82aa92d483671bc5945d
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-23 10:43:46 +0600

    Added catching to getallproducts controller.

--- src/Services/Catalog.API/Controllers/CatalogController.cs
+++ src/Services/Catalog.API/Controllers/CatalogController.cs
@@ -18,6 +18,7 @@ namespace Catalog.API.Controllers
 
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 10)]
         public async Task<IActionResult> GetAll()
         {
             try
