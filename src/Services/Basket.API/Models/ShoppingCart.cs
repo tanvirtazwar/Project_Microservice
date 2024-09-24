50a18ce240fe52c45aeb472b6d6b9c5db345aa49
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-24 18:01:53 +0600

    Added models to basket.api.

---
+++ src/Services/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = null!;
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public ShoppingCart() { }
+    }
+}
