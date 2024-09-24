e29ca7bac0a9ef8f4058353d1fa6c19296558dc3
tanvirtazwar <tanvirtazwar37@gmail.com>
2024-09-25 03:01:08 +0600

    Added basket controller.

---
+++ src/Services/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,73 @@
+﻿using Basket.API.Models;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket(string userName)
+        {
+            try
+            {
+                var basket = await basketRepository.GetBasket(userName);
+                if (basket == null)
+                {
+                    return NotFound("User not found.");
+                }
+                return Ok(basket);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart shoppingCart)
+        {
+            try
+            {
+                var basket = await basketRepository.UpdateBasket(shoppingCart);
+                if (basket == null)
+                {
+                    return NotFound("User not found.");
+                }
+                return Ok(basket);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket (string userName)
+        {
+            try
+            {
+                var basket = await basketRepository.GetBasket(userName);
+                if (basket == null)
+                {
+                    return NotFound("User not found.");
+                }
+                await basketRepository.DeleteBasket(userName);
+                return Ok("Basket has been deleted.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,14 +1,20 @@
+using Basket.API.Repositories;
+using Basket.API.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddStackExchangeRedisCache
-(options => 
-{
-    var configure = builder.Configuration;
-    options.Configuration = configure.GetConnectionString("BasketDB");
-}
+(
+    options =>
+    {
+        var configure = builder.Configuration;
+        options.Configuration = configure.GetConnectionString("BasketDB");
+    }
 );
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
